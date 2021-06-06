f56fff7012d393dfbfc1d0bef6280923b56ab9e5
Aret Mimaryan <amimaryan@gmail.com>
2021-06-06 17:45:53 -0400

    break elements into components for Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App"> 
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Home</a>
-          <div>
-            <ul className="navbar-nav me-auto mb-2 mb-md-0">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Login</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Register</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
+    <Nav />
       <main className="form-signin">
-        <form>
-          <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
-          <div className="form-floating">
-            <input type="email" className="form-control" placeholder="name@example.com" />
-          </div>
-          <div className="form-floating">
-            <input type="password" className="form-control" placeholder="Password" />
-          </div>
-          <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-        </form>
+        <Login />
       </main>
     </div>
   );
---
+++ src/pages/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <form>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            <div className="form-floating">
+                <input type="email" className="form-control" placeholder="name@example.com" />
+            </div>
+            <div className="form-floating">
+                <input type="password" className="form-control" placeholder="Password" />
+            </div>
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    );
+}
+
+export default Login;
